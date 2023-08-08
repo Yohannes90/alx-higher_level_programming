a26bd91e79e0baba4a66af61657b697049a6745b
Yohannes90 <mekonnenyohannes90@gmail.com>
2023-08-08 23:06:56 +0300

    second commit for question 8 (8-uppercase.py) with pycodestyle check

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -4,5 +4,5 @@ def uppercase(str):
     for ch in str:
         if ord(ch) in range(97, 123):
             ch = chr(ord(ch) - 32)
-        print("{:s}".format(ch), end="")     
+        print("{:s}".format(ch), end="")
     print("")
