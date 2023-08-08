5c6bbd81ba790c205769791806b2a24145d7e065
Yohannes90 <mekonnenyohannes90@gmail.com>
2023-08-08 23:05:34 +0300

    second commit for question 8 (8-uppercase.py)

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def uppercase(str):
+    for ch in str:
+        if ord(ch) in range(97, 123):
+            ch = chr(ord(ch) - 32)
+        print("{:s}".format(ch), end="")     
+    print("")
