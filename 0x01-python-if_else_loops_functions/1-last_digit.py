de7ac92073c70ac7442916b8b8297ceb57c2fc69
Yohannes90 <mekonnenyohannes90@gmail.com>
2023-08-08 18:02:24 +0300

    second commit for question 1 correction in handling modulo of negative numbers

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-last = number % 10
+if number < 0:
+    last = -(number) % 10
+else:
+    last = number % 10
 if last > 5:
     print(f"Last digit of {number} is {last} and is greater than 5")
 elif last == 0:
