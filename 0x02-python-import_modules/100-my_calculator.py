ecc78c17cafc599331904a17a08e34fb2cf12680
Yohannes90 <mekonnenyohannes90@gmail.com>
2023-08-10 22:27:45 +0300

    second commit for question 6 correction of pycodestyle and others

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,20 +3,20 @@ if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     from sys import argv, exit
     if len(argv) != 4:
-        print("Unknown operator. Available operators: +, -, * and /")
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-    operand_1 = int(argv[1])
-    operand_2 = int(argv[3])
+    op1 = int(argv[1])
+    op2 = int(argv[3])
     operator = argv[2]
     match operator:
         case '+':
-            print("{} + {} = {}".format(operand_1, operand_2, add(operand_1, operand_2)))
+            print("{} + {} = {}".format(op1, op2, add(op1, op2)))
         case '-':
-            print("{} - {} = {}".format(operand_1, operand_2, sub(operand_1, operand_2)))
+            print("{} - {} = {}".format(op1, op2, sub(op1, op2)))
         case '*':
-            print("{} * {} = {}".format(operand_1, operand_2, (operand_1 * operand_2)))
+            print("{} * {} = {}".format(op1, op2, mul(op1 * op2)))
         case '/':
-            print("{} / {} = {}".format(operand_1, operand_2, div(operand_1, operand_2)))
+            print("{} / {} = {}".format(op1, op2, div(op1, op2)))
         case _:
             print("Unknown operator. Available operators: +, -, * and /")
             exit(1)
