643ea827bd8636c7b8387f8a9690e43c558e8e8b
Yohannes90 <mekonnenyohannes90@gmail.com>
2023-08-10 21:55:58 +0300

    second commit for question 2, correction of print format for 0 and 1 argument

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from sys import argv
-    num = len(argv)
-    print("{} arguments.".format(num - 1))
-    for i in range(1, num):
-        print("{}: {}".format(i, argv[i]))
+    num = len(argv) - 1
+    if num == 0:
+        print("0 arguments.")
+    elif num == 1:
+        print("1 argument:\n1: {}".format(argv[1]))
+    else:
+        print("{} arguments:".format(num))
+        for i in range(1, num + 1):
+            print("{}: {}".format(i, argv[i]))
