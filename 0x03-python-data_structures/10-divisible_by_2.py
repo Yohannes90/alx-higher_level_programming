73b9b9b41db9154642c23b90639d29af7ecf295e
Yohannes90 <mekonnenyohannes90@gmail.com>
2023-08-13 22:36:39 +0300

    second commit for question 10

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -2,8 +2,8 @@
 
 def divisible_by_2(my_list=[]):
     new_list = []
-    for idx in my_list:
-        if my_list[idx] % 2 == 0:
+    for i in my_list:
+        if i % 2 == 0:
             new_list.append(True)
         else:
             new_list.append(False)
