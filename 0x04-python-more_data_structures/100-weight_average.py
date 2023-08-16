0b6a2e3df58aab2cb547a007b881945ae0a7c1d3
Yohannes90 <mekonnenyohannes90@gmail.com>
2023-08-16 23:58:01 +0300

    first commit for question 13

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def weight_average(my_list=[]):
+    if not my_list:
+        return 0
+    total = 0
+    sum = 0
+    for i in my_list:
+        total += i[0] * i[1]
+        sum += i[1]
+    return total / sum
