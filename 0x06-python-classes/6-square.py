cc5857bb2f6f63999fb345f00ab4e3118fc7ebac
Yohannes90 <mekonnenyohannes90@gmail.com>
2023-08-30 00:10:44 +0300

    fourth commit for question 6, correction on handling positive numbers i position

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -33,7 +33,7 @@ class Square:
     def position(self, value):
         if (not isinstance(value, tuple)) or (len(value) != 2):
             raise TypeError("position must be a tuple of 2 positive integers")
-        if value[0] >= 0 and value[1] >= 0:
+        if value[0] <= 0 and value[1] <= 0:
             raise TypeError("position must be a tuple of 2 positive integers")
         self.position = value
 
