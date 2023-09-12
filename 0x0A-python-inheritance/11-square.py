3471aad5f3d31bcd21f4642ff8c977cb92e59036
Yohannes90 <mekonnenyohannes90@gmail.com>
2023-09-13 01:08:57 +0300

    first commit for question 11

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+'''Defines a square class'''
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    '''This class represents square class'''
+    def __init__(self, size):
+        """Initalize square object"""
+        self.__size = size
+        super().__init__(self.__size, self.__size)
+
+    def area(self):
+        return super().area()
+
+    def __str__(self):
+        """String representation of rectangle object"""
+        return ("[Square] {}/{}".format(self.__size, self.__size))
