7771bcf07fb0074ea685f8a65cfbae467a98b970
Yohannes90 <mekonnenyohannes90@gmail.com>
2023-09-12 23:31:16 +0300

    first commit for question 8

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''
+Contains a function that returns the dictionary description
+with simple data structure (list, dictionary, string,
+integer and boolean) for JSON serialization of an object
+'''
+
+
+def class_to_json(obj):
+    '''
+    returns the dictionary description
+    for JSON serialization of an object
+    '''
+    return obj.__dict__
