a5a07272abeaea084883abefaf794ee70afd57c8
Yohannes90 <mekonnenyohannes90@gmail.com>
2023-09-12 23:58:33 +0300

    first commit for question 10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Describes Student"""
+
+
+class Student:
+    """Representation of Student"""
+    def __init__(self, first_name, last_name, age):
+        """Initalizes student object"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Return dicionary representatuon of student object"""
+        if (type(attrs) is list and
+                all(type(ele) is str for ele in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
