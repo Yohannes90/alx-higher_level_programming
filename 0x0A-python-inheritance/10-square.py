5285a167a1d8f0c89f59a3b47104142383e683a5
Yohannes90 <mekonnenyohannes90@gmail.com>
2023-09-13 00:54:52 +0300

    first commit for question 10

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+'''Defines a square class'''
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    '''This class represents square class'''
+    def __init__(self, size):
+        """Initalize square object"""
+        self.__size = size
+        super().__init__(self.__size, self.__size)
