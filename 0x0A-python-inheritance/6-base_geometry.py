8071ee44adb9e1c25a1639af32161de3ec232f02
Yohannes90 <mekonnenyohannes90@gmail.com>
2023-09-12 04:23:45 +0300

    first commit for question 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Defines a base geometry class"""
+
+
+class BaseGeometry:
+    """This class represents base geometry class"""
+    def area(self):
+        """to be implemented area method"""
+        raise Exception("area() is not implemented")
