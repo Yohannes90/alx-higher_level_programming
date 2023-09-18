65569da2a8c18d721067f77ab261a64e23f7ada2
Yohannes90 <mekonnenyohannes90@gmail.com>
2023-09-18 23:09:34 +0300

    adding test files for rectangle

---
+++ 0x0C-python-almost_a_circle/tests/rectangle_unitest.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Module containing test the functions for rectangle"""
+import unittest
+import rectangle
+
+
+class TestRectangle(unittest.TestCase):
+    """Representaton of test rectangle"""
+    def test_rectangle_output(self):
+        """Function to test the output the function rectangle"""
+        self.assertAlmostEqual(rectangle(,)
+
+    def test_rectangle_input(self):
+        """Function to test the input to rectangle"""
+        self.assertRaises(TypeError, rectangle, )
---
+++ 0x0C-python-almost_a_circle/tests/test_rectangle.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Unit test for Rectangle class"""
+import os
+import sys
+import json
+import unittest
+from models.rectangle import Rectangle
