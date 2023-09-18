d0caf838f71034666a55c86f83b6481f4899bc60
Yohannes90 <mekonnenyohannes90@gmail.com>
2023-09-18 23:42:36 +0300

    pycodecorrection on rectangle class for question 6.__str__

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -83,10 +83,10 @@ class Rectangle(Base):
     def __str__(self):
         """String representation of rectangle object"""
         return ("[Rectangle] ({}) {}/{} - {}/{}".format(self.id,
-                                                      self.x,
-                                                      self.y,
-                                                      self.width,
-                                                      self.height))
+                                                        self.x,
+                                                        self.y,
+                                                        self.width,
+                                                        self.height))
 
     def update(self, *args, **kwargs):
         '''Assigns/updates an argument to each rectangle attribute'''
