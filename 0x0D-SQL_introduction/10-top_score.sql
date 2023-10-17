91ba2bc8a1bc94392ef59023af6a2c1f64e1346e
Yohannes90 <mekonnenyohannes90@gmail.com>
2023-10-17 23:21:51 +0300

    third commit for question 10

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,3 +1,4 @@
--- lists all records of the table second_table of the database hbtn_0c_0 in MySQL server
+-- lists all records of the table second_table of the database
 
-SELECT * FROM `second_table`;
+SELECT *
+FROM `second_table`;
