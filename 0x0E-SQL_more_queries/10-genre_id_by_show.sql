254712a0a93d720e0bf87e4e73388c762cf6e221
Yohannes90 <mekonnenyohannes90@gmail.com>
2023-10-18 23:41:16 +0300

    first commit for question 10

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,8 @@
+--  lists all shows contained in hbtn_0d_tvshows that have at least one genre linked
+    -- display: tv_shows.title - tv_show_genres.genre_id
+    -- sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
+
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows, tv_show_genres
+WHERE tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
