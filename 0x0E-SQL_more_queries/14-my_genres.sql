93064a3d19e0fdb09b655631c715a274c788cc11
Yohannes90 <mekonnenyohannes90@gmail.com>
2023-10-19 00:45:20 +0300

    first commit for question 14

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,12 @@
+-- uses the hbtn_0d_tvshows database to lists all genres of the show Dexter
+    -- tv_shows table contains only one record where title = Dexter
+        -- (but the id can be different)
+    -- display: tv_genres.name
+    -- sorted in ascending order by the genre name
+    -- You can use only one SELECT statement
+
+SELECT tv_genres.name
+FROM tv_genres INNER JOIN tv_show_genres
+    ON tv_genres.id = tv_show_genres.genre_id INNER JOIN tv_shows
+        ON tv_show_genres.show_id = tv_shows.id
+WHERE tv_shows.title = "Dexter"
