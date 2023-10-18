fa124031b0eddb1bf7829af5c714c45c76aa0b84
Yohannes90 <mekonnenyohannes90@gmail.com>
2023-10-18 23:08:57 +0300

    second commit for question 8, correction tring to fix checker errors by changing my query from eqi-join to sub-query

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,5 +1,7 @@
 -- lists all the cities of California that can be found in the database hbtn_0d_usa
 
-SELECT c.id, c.name
-FROM cities AS c, states AS s
-WHERE c.state_id = s.id;
+SELECT id, name
+FROM cities
+WHERE state_id = (SELECT id
+                  FROM states
+                  WHERE name = "California");
