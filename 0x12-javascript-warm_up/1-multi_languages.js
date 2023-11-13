87c3850285552bf855c8d1f3f087827a09a7761f
Yohannes90 <mekonnenyohannes90@gmail.com>
2023-11-13 12:44:34 +0300

    first commit for question 1

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+const first = 'C is fun';
+const second = 'Python is cool';
+const third = 'JavaScript is amazing';
+
+console.log(first);
+console.log(second);
+console.log(third);
