0c85353605bfc072ae991b0e7e3b6fc0bf3a2fcb
Yohannes90 <mekonnenyohannes90@gmail.com>
2023-11-17 14:25:21 +0300

    third commit for question 1, correction on adding documentation

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
-
+""" lists all states from the database hbtn_0e_0_usa
+"""
 import MySQLdb
-import sys
+from sys import argv
 
 if __name__ == "__main__":
-    conn = MySQLdb.connect(host='localhost', user=sys.argv[1],
-                           passwd=sys.argv[2], db=sys.argv[3], port=3306)
+
+    conn = MySQLdb.connect(
+        host="localhost", user=argv[1], passwd=argv[2], db=argv[3], port=3306
+    )
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%'")
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%';")
     rows = cur.fetchall()
     for row in rows:
         print(row)
