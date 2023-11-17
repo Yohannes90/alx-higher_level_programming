68c653b38f6120babae7117b54774eeeecb94d72
Yohannes90 <mekonnenyohannes90@gmail.com>
2023-11-17 14:36:22 +0300

    fourth commit for question 1, correction on using like rather than binary like which causes the search in both upper and lower case rather than upper case only

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" lists all states from the database hbtn_0e_0_usa
+""" lists all states with name starting with N(upper N) from hbtn_0e_0_usa
 """
 import MySQLdb
 from sys import argv
@@ -10,7 +10,7 @@ if __name__ == "__main__":
         host="localhost", user=argv[1], passwd=argv[2], db=argv[3], port=3306
     )
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%';")
+    cur.execute("SELECT * FROM states WHERE name BINARY LIKE 'N%';")
     rows = cur.fetchall()
     for row in rows:
         print(row)
