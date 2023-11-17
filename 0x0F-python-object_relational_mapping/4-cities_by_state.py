a6a32a5523e3c3dfb1614c894a901d62df59e720
Yohannes90 <mekonnenyohannes90@gmail.com>
2023-11-17 18:14:50 +0300

    second commit for question 4, correction on adding documentation and code clarity

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
-
+""" lists all cities from the database hbtn_0e_4_usa
+"""
 import MySQLdb
-import sys
-
+from sys import argv
 
 if __name__ == "__main__":
-    conn = MySQLdb.connect(host='localhost', user=sys.argv[1],
-                           passwd=sys.argv[2], db=sys.argv[3], port=3306)
+
+    conn = MySQLdb.connect(
+        host="localhost", user=argv[1], passwd=argv[2], db=argv[3], port=3306
+    )
     cur = conn.cursor()
     cur.execute("SELECT cities.id, cities.name, states.name FROM cities,\
-                 states WHERE cities.state_id = states.id ORDER BY id ASC")
+                 states WHERE cities.state_id = states.id ORDER BY id ASC;")
     rows = cur.fetchall()
     for row in rows:
         print(row)
