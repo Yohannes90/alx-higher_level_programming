d71066d2e98f667721dbe2d549bfed46694d6836
Yohannes90 <mekonnenyohannes90@gmail.com>
2023-11-17 18:23:10 +0300

    second commit for question 5, correction on adding documentation and code clarity

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,15 +1,19 @@
 #!/usr/bin/python3
-
+""" takes in the name of a state as an argument and lists all cities of that
+    state, using the database hbtn_0e_4_usa
+"""
 import MySQLdb
-import sys
+from sys import argv
 
 if __name__ == "__main__":
-    conn = MySQLdb.connect(host='localhost', user=sys.argv[1],
-                           passwd=sys.argv[2], db=sys.argv[3], port=3306)
+
+    conn = MySQLdb.connect(
+        host="localhost", user=argv[1], passwd=argv[2], db=argv[3], port=3306
+    )
     cur = conn.cursor()
-    state = sys.argv[4]
-    cur.execute("SELECT cities.name FROM cities, states WHERE\
-            cities.state_id = states.id AND states.name LIKE %s", (state, ))
+    state = argv[4]
+    cur.execute("SELECT cities.name FROM cities, states WHERE cities.state_id\
+                 = states.id AND states.name LIKE BINARY %s", (state, ))
 
     rows = cur.fetchall()
     print(", ".join([row[0] for row in rows]))
