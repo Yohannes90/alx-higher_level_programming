ff9a30aead80065ee57c66daeadad84ee2b8a24b
Yohannes90 <mekonnenyohannes90@gmail.com>
2023-11-17 14:38:05 +0300

    fourth commit for question 1, correction on syntax LIKE BINARY rather than BINARY LIKE

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
         host="localhost", user=argv[1], passwd=argv[2], db=argv[3], port=3306
     )
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states WHERE name BINARY LIKE 'N%';")
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%';")
     rows = cur.fetchall()
     for row in rows:
         print(row)
