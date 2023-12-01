5b93c342fca21bc1a57b2ca653e057c60da28508
Yohannes90 <mekonnenyohannes90@gmail.com>
2023-12-01 22:53:32 +0300

    first commit for question 6

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" takes in URL and an email, sends a POST request to the passed URL with the
+    email as a parameter, displays the body of the response
+"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    r = requests.get(sys.argv[1], {"email": sys.argv[2]})
+    print(r.text)
