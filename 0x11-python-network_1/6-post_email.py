b3d3539ea3a60a6552ea0d11435cc6cc384fa52e
Yohannes90 <mekonnenyohannes90@gmail.com>
2023-12-01 22:56:12 +0300

    second commit for question 6

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -7,5 +7,5 @@ import requests
 import sys
 
 if __name__ == "__main__":
-    r = requests.get(sys.argv[1], {"email": sys.argv[2]})
+    r = requests.post(sys.argv[1], {"email": sys.argv[2]})
     print(r.text)
