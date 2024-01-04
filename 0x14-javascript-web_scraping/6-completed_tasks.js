ce8bd45b2085dcaba910da329d51b74e3e3cfbdc
Yohannes90 <mekonnenyohannes90@gmail.com>
2024-01-04 22:17:45 +0300

    second commit for question 6, correction on iterators and ternery operator

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -5,8 +5,13 @@ const url = process.argv[2];
 request(url, (error, response, body) => {
   if (!error) {
     const todos = JSON.parse(body);
-    const completed = todos.filter(task => task.completed).forEach((todo) => {
-      completed[todo.userId] === undefined ? completed[todo.userId] = 1 : completed[todo.userId] += 1;
+    const completed = {};
+    todos.filter(task => task.completed).forEach((todo) => {
+      if (completed[todo.userId] === undefined) {
+        completed[todo.userId] = 1;
+      } else {
+        completed[todo.userId] += 1;
+      }
     });
     console.log(completed);
   }
