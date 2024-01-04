8fb8ca2a96ad5d8b65ec8019278b0277a3590b82
Yohannes90 <mekonnenyohannes90@gmail.com>
2024-01-04 23:09:45 +0300

    third commit for question 7, correction on for each method syntax

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -5,11 +5,9 @@ const url = `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`;
 request(url, (error, response, body) => {
   if (!error) {
     const characters = JSON.parse(body).characters;
-    characters.foreach(char => {
-      request(char, (error, response, body) => {
-        if (!error) {
-          console.log(JSON.parse(body).name);
-        }
+    characters.forEach((character) => {
+      request(character, (error, response, body) => {
+        console.log(error || JSON.parse(body).name);
       });
     });
   }
